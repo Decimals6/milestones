f077637dc4fb31aa2a356cda0383ff80951ec9d8
awokawokwkwkw <147193334+quaso-hub@users.noreply.github.com>
2025-06-25 19:46:53 +0700

    papa mau 3

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,13 +9,16 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        $this->call([
-            CustomersTableSeeder::class,
-            TransactionsTableSeeder::class,
-            PaymentsTableSeeder::class,
-        ]);
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
